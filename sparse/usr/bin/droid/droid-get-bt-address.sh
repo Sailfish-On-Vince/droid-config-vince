e618fdbdbd0cbc155ffad12caabcdc98e562507b
BirdZhang <0312birdzhang@gmail.com>
2022-08-16 06:03:56 +0000

    fix typo
    Change-Id: Ic63388529fc764a2aed726e4adbb6177cfd0d7c7

--- sparse/usr/bin/droid/droid-get-bt-address.sh
+++ sparse/usr/bin/droid/droid-get-bt-address.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "droid-get-bt-address: Setting up bluetooth address"
-bt_mac=$(cat data/misc/bluedroid/bt_config.conf|grep Address|awk '{print $NF}')
-echo $bt_mac > /var/lib/bluetooth/board-address
+if [ ! -f /var/lib/bluetooth/board-address ];then
+  bt_mac=$(cat /data/misc/bluedroid/bt_config.conf|grep Address|awk '{print $NF}')
+  echo $bt_mac > /var/lib/bluetooth/board-address
+fi
