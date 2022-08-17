38726d5d4540f58094f7999408195f81401ab2af
BirdZhang <0312birdzhang@gmail.com>
2022-08-17 07:57:43 +0000

    add back droid-hcismd-up.sh

--- sparse/usr/bin/droid/droid-get-bt-address.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "droid-get-bt-address: Setting up bluetooth address"
-if [ ! -f /var/lib/bluetooth/board-address ];then
-  bt_mac=$(cat /data/misc/bluedroid/bt_config.conf|grep Address|awk '{print $NF}')
-  echo $bt_mac > /var/lib/bluetooth/board-address
-fi
